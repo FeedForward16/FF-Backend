058a00e83fdd7e1e9986be03a3556d8da5c5ec2a
Sravya-Lahari <sravya@zoommylife.com>
2025-05-11 14:04:42 +0530

    message

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-console.log("hello");
-console.log('sravya');
+const express=require("express");
+const app=express();
+
+const port=5000;
+
+app.use(express.json());
+app.use("/user",require("./routes/userRoutes"));
+app.listen(port,()=>{
+    console.log("server running on port ",port);
+});
